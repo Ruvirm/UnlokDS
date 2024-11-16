e037da3e98a713633b29f11873c72a6ad800c17e
Ruvirm <140802204+Ruvirm@users.noreply.github.com>
2024-11-16 14:21:27 +0300

    Add files via upload

---
+++ UnlokDS1.2.bat
@@ -0,0 +1,115 @@
+@echo off
+REM Удаляем все маршруты, добавленные для блокировки Discord
+route -p delete 104.16.0.0 mask 255.255.0.0
+route -p delete 104.17.0.0 mask 255.255.0.0
+route -p delete 104.18.0.0 mask 255.255.0.0
+route -p delete 104.19.0.0 mask 255.255.0.0
+route -p delete 104.20.0.0 mask 255.255.0.0
+route -p delete 104.21.0.0 mask 255.255.0.0
+route -p delete 104.22.0.0 mask 255.255.0.0
+route -p delete 104.23.0.0 mask 255.255.0.0
+route -p delete 104.24.0.0 mask 255.255.0.0
+route -p delete 104.25.0.0 mask 255.255.0.0
+route -p delete 104.26.0.0 mask 255.255.0.0
+route -p delete 104.27.0.0 mask 255.255.0.0
+route -p delete 104.28.0.0 mask 255.255.0.0
+route -p delete 104.29.0.0 mask 255.255.0.0
+route -p delete 104.30.0.0 mask 255.255.0.0
+route -p delete 104.31.0.0 mask 255.255.0.0
+route -p delete 108.61.0.0 mask 255.255.0.0
+route -p delete 209.222.0.0 mask 255.255.255.0
+route -p delete 209.222.1.0 mask 255.255.255.0
+route -p delete 209.222.2.0 mask 255.255.255.0
+route -p delete 209.222.3.0 mask 255.255.255.0
+route -p delete 209.222.4.0 mask 255.255.255.0
+route -p delete 209.222.5.0 mask 255.255.255.0
+route -p delete 209.222.6.0 mask 255.255.255.0
+route -p delete 209.222.7.0 mask 255.255.255.0
+route -p delete 209.222.8.0 mask 255.255.255.0
+route -p delete 209.222.9.0 mask 255.255.255.0
+route -p delete 209.222.10.0 mask 255.255.255.0
+route -p delete 209.222.11.0 mask 255.255.255.0
+route -p delete 209.222.12.0 mask 255.255.255.0
+route -p delete 209.222.13.0 mask 255.255.255.0
+route -p delete 209.222.14.0 mask 255.255.255.0
+route -p delete 209.222.15.0 mask 255.255.255.0
+route -p delete 209.222.16.0 mask 255.255.255.0
+route -p delete 209.222.17.0 mask 255.255.255.0
+route -p delete 209.222.18.0 mask 255.255.255.0
+route -p delete 209.222.19.0 mask 255.255.255.0
+route -p delete 209.222.20.0 mask 255.255.255.0
+route -p delete 209.222.21.0 mask 255.255.255.0
+route -p delete 209.222.22.0 mask 255.255.255.0
+route -p delete 209.222.23.0 mask 255.255.255.0
+route -p delete 209.222.24.0 mask 255.255.255.0
+route -p delete 209.222.25.0 mask 255.255.255.0
+route -p delete 209.222.26.0 mask 255.255.255.0
+route -p delete 209.222.27.0 mask 255.255.255.0
+route -p delete 209.222.28.0 mask 255.255.255.0
+route -p delete 209.222.29.0 mask 255.255.255.0
+route -p delete 209.222.30.0 mask 255.255.255.0
+route -p delete 209.222.31.0 mask 255.255.255.0
+route -p delete 213.196.52.0 mask 255.255.255.0
+route -p delete 213.196.53.0 mask 255.255.255.0
+route -p delete 213.196.54.0 mask 255.255.255.0
+route -p delete 213.196.55.0 mask 255.255.255.0
+route -p delete 192.111.128.0 mask 255.255.255.0
+route -p delete 192.111.129.0 mask 255.255.255.0
+route -p delete 192.111.130.0 mask 255.255.255.0
+route -p delete 192.111.131.0 mask 255.255.255.0
+route -p delete 192.111.132.0 mask 255.255.255.0
+route -p delete 192.111.133.0 mask 255.255.255.0
+route -p delete 192.111.134.0 mask 255.255.255.0
+route -p delete 192.111.135.0 mask 255.255.255.0
+route -p delete 192.111.136.0 mask 255.255.255.0
+route -p delete 192.111.137.0 mask 255.255.255.0
+route -p delete 192.111.138.0 mask 255.255.255.0
+route -p delete 192.111.139.0 mask 255.255.255.0
+route -p delete 192.111.140.0 mask 255.255.255.0
+route -p delete 192.111.141.0 mask 255.255.255.0
+route -p delete 192.111.142.0 mask 255.255.255.0
+route -p delete 192.111.143.0 mask 255.255.255.0
+route -p delete 23.105.224.0 mask 255.255.255.0
+route -p delete 23.105.225.0 mask 255.255.255.0
+route -p delete 23.105.226.0 mask 255.255.255.0
+route -p delete 23.105.227.0 mask 255.255.255.0
+route -p delete 23.105.228.0 mask 255.255.255.0
+route -p delete 23.105.229.0 mask 255.255.255.0
+route -p delete 23.105.230.0 mask 255.255.255.0
+route -p delete 23.105.231.0 mask 255.255.255.0
+route -p delete 23.105.232.0 mask 255.255.255.0
+route -p delete 23.105.233.0 mask 255.255.255.0
+route -p delete 23.105.234.0 mask 255.255.255.0
+route -p delete 23.105.235.0 mask 255.255.255.0
+route -p delete 23.105.236.0 mask 255.255.255.0
+route -p delete 23.105.237.0 mask 255.255.255.0
+route -p delete 23.105.238.0 mask 255.255.255.0
+route -p delete 23.105.239.0 mask 255.255.255.0
+route -p delete 23.105.240.0 mask 255.255.255.0
+route -p delete 23.105.241.0 mask 255.255.255.0
+route -p delete 23.105.242.0 mask 255.255.255.0
+route -p delete 23.105.243.0 mask 255.255.255.0
+route -p delete 23.105.244.0 mask 255.255.255.0
+route -p delete 23.105.245.0 mask 255.255.255.0
+route -p delete 23.105.246.0 mask 255.255.255.0
+route -p delete 23.105.247.0 mask 255.255.255.0
+
+REM Добавьте остальные IP-адреса из оригинального скрипта аналогично выше
+
+REM Удаляем изменения в файле hosts
+cd %userprofile%
+if exist hosts (
+    del hosts
+)
+echo Оригинальный файл hosts восстанавливается...
+copy /Y %windir%\system32\drivers\etc\hosts %userprofile%\hosts
+copy /Y %userprofile%\hosts %windir%\system32\drivers\etc\hosts
+
+REM Удаляем записи реестра, блокирующие Discord
+reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\" /v DisallowRun /f
+reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\DisallowRun" /v 1 /f
+reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\run" /v BDS /f
+
+REM Уведомление об успешной разблокировке
+echo Discord разблокирован.
+pause
